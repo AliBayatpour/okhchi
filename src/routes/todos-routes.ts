3391b766a6807c559a8e335abf5570f966f2b7fa
Ali Bayatpour <aliglbayatpour@gmail.com>
2023-08-25 14:25:29 +0300

    error

--- src/routes/todos-routes.ts
+++ src/routes/todos-routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import checkAuth from "../middlewares/check-auth";
+import todosControllers from "../controellers/todos-controllers";
 
 const router = Router();
 router.use(checkAuth);
 
 
-router.get("", TodosController.getTodosByUserId);
-router.post("", TodosController.createTodo);
-router.put("", TodosController.updateTodo);
-router.delete("/:id", TodosController.deleteTodo);
+router.get("", todosControllers.getTodosByUserId);
+router.post("", todosControllers.createTodo);
+router.put("", todosControllers.updateTodo);
+router.delete("/:id", todosControllers.deleteTodo);
 
 export default router;
